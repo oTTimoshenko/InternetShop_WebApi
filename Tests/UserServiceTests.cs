13f1cb4deee358d58ac2d224fefa657875412f3f
Anton Suhorada <AntonA.Suhorada@livenau.net>
2018-05-26 01:20:20 +0300

    Unit tests allmost done

--- Tests/UserServiceTests.cs
+++ Tests/UserServiceTests.cs
@@ -9,6 +9,7 @@ using BLL.Services;
 using System.Collections.Generic;
 using UoWandRepositories.Entities;
 using BLL.DTO;
+using BLL.Entity;
 
 namespace Tests
 {
@@ -25,6 +26,85 @@ namespace Tests
             _service = new UserService(_db.Object, _mapper);
         }
 
+        [Test]
+        public void AddItemToCartTest()
+        {
+            _db.Setup(x => x.Items.Add(It.IsAny<ItemUoW>())).Verifiable();
 
+            var itemDTO = _mapper.Map<ItemDTO>(GetItem());
+            bool act = _service.AddItem(itemDTO, 2, GetCart());
+
+            Assert.IsTrue(act);
+        }
+
+        [Test]
+        public void RemoveItemFromCartTest()
+        {
+            bool act = _service.RemoveItem(GetItem() ,GetCart());
+
+            Assert.IsTrue(act);
+        }
+
+        public ShoppingCart GetCart()
+        {
+            return new ShoppingCart()
+            {
+                lines = new List<ShoppingCartLine>()
+                {
+                    new ShoppingCartLine()
+                    {
+                        Item = new ItemDTO
+                        {
+                            ItemId = 1,
+                            ItemName = "IPhone 5",
+                            CategoryId = 1,
+                            Price = 500,
+                        },
+                        Quantity = 1,
+                        ShoppingCartId = 0
+                    }
+                },
+                overallPrice = 0
+            };
+
+        }
+
+        public ItemDTO GetItem()
+        {
+            return new ItemDTO
+            {
+                ItemId = 1,
+                ItemName = "IPhone 5",
+                CategoryId = 1,
+                Price = 500,
+            };
+        }
+       
+        [Test]
+        public void ClearCartTest()
+        {
+            bool act = _service.Clear(GetCart());
+
+            Assert.IsTrue(act);
+        }
+
+        //[Test]
+        //public void ComposeCartTest()
+        //{
+        //    _db.Setup(x => x.Items.Add(It.IsAny<ItemUoW>())).Verifiable();
+
+        //    var act = _service.ComposeCart(GetCart());
+
+        //    var exp = GetCart();
+
+        //    Assert.AreEqual(act, exp);
+        //}
+
+        //[Test]
+        //public void MakeOrderTest()
+        //{
+
+        //}
     }
+
 }
